5430990e6996d99facbdc330363435b84baf77e5
rupinwhitehatjr <61224617+rupinwhitehatjr@users.noreply.github.com>
2020-04-10 15:46:56 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -8,17 +8,13 @@ class dustbin
 		this.dustbinHeight=100;
 		this.wallThickness=20;
 		this.angle=-20;	
-		var render= {
-         fillStyle: 'red',
-         strokeStyle: 'blue',
-         lineWidth: 3
-    	}	
-		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true},render)
-		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true},render)
+		
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
 		Matter.Body.setAngle(this.leftWallBody, this.angle);
 		
 
-		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true},render)
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
 		Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
 		World.add(world, this.bottomBody)
 		World.add(world, this.leftWallBody)
