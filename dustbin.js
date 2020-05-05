06ef3c918509a2ed71c5dab0aa2ffd39cc26853f
rupinwhitehatjr <61224617+rupinwhitehatjr@users.noreply.github.com>
2020-05-05 15:38:14 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -7,7 +7,7 @@ class dustbin
 		this.dustbinWidth=200;
 		this.dustbinHeight=100;
 		this.wallThickness=20;
-		this.angle=-0.3;	
+		this.angle=0;	
 		
 		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
 		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
@@ -35,6 +35,7 @@ class dustbin
 			//strokeWeight(4);
 			angleMode(RADIANS)
 			fill(255)
+			stroke(255)
 			rotate(this.angle)
 			rect(0,0,this.wallThickness, this.dustbinHeight);
 			pop()
@@ -43,6 +44,7 @@ class dustbin
 			translate(posRight.x, posRight.y);
 			rectMode(CENTER)
 			//strokeWeight(4);
+			stroke(255)
 			angleMode(RADIANS)
 			fill(255)
 			rotate(-1*this.angle)
@@ -52,7 +54,8 @@ class dustbin
 			push()
 			translate(posBottom.x, posBottom.y);
 			rectMode(CENTER)
-			strokeWeight(4);
+			//strokeWeight(4);
+			stroke(255)
 			angleMode(RADIANS)
 			fill(255)
 			rect(0,0,this.dustbinWidth, this.wallThickness);
--- paper.js
+++ paper.js
@@ -25,7 +25,7 @@ class paper
 			push()
 			translate(paperpos.x, paperpos.y);
 			rectMode(CENTER)
-			strokeWeight(4);
+			strokeWeight(3);
 			fill(255,0,255)
 			ellipse(0,0,this.r, this.r);
 			pop()
